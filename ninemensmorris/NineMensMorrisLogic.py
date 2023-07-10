8a66e88a39463f621010c1cdc150215cce979eb6
LittleH0rst <johnny.captin@gmail.com>
2023-07-10 18:25:38 +0200

    added checks - checkpoint

--- ninemensmorris/NineMensMorrisLogic.py
+++ ninemensmorris/NineMensMorrisLogic.py
@@ -13,7 +13,9 @@ Changes: JJ, July 05th, changes to board properties, logic for legal moves on cu
 
 Changes: JJ, July 07th, logic for legal moves, finished game logic for phase 0
 
-Changes: JJ, July 08th, logic for legal moves, phases 1 and 2, adaption for mill check function
+Changes: JJ, July 08th, logic for legal moves, adaption for mill check function
+
+Changes: JJ, July 10th, logic for legal moves, phases 1 and 2
 '''
 import numpy as np
 
@@ -73,6 +75,8 @@ s
         for a action, we put a string "none".
         
         """
+        moves = get_all_moves_phase_zero(self) + get_all_moves_phase_one_and_two(self)
+        
         
         return list(moves)
         
@@ -223,6 +227,9 @@ s
     """
     identifies possible mills, checking if any of the moves on the current board would
     form a mill (results in a different marking for the list of all moves)
+    move_locations => Array of Tuples (origin, move) 
+    Each check makes sure, that the origin of the move, isnt one of the pieces in the
+    potentially new mill
     """
     def get_possible_mills(self, move_locations, player):
         
@@ -230,57 +237,75 @@ s
         
         for move in move_locations:
             if move != None:
-                if move % 2 == 0: #move is in a corner
-                    if move % 8 == 0: # move is in the top left corner of a ring
-                        if (self.pieces[move + 7] == player and self.pieces[move + 6] == player): #check down
+                if move[1] % 2 == 0: #move is in a corner
+                    if move[1] % 8 == 0: # move is in the top left corner of a ring
+                        if (self.pieces[move[1] + 7] == player and self.pieces[move[1] + 6] == player and 
+                           move[1] + 7 != move[0] and move[1] + 6 != move[0]): #check down
                             move_forms_mill.append(move)
-                        if (self.pieces[move + 1] == player and self.pieces[move + 2] == player): #check right
+                        if (self.pieces[move[1] + 1] == player and self.pieces[move[1] + 2] == player and
+                           move[1] + 1 != move[0] and move[1] + 2 != move[0]): #check right
                             move_forms_mill.append(move)
                     elif move in [6,14,22]: #move is in the bottom left corner of a ring
-                        if (self.pieces[move + 1] == player and self.pieces[move - 6] == player): #check up
+                        if (self.pieces[move[1] + 1] == player and self.pieces[move[1] - 6] == player and 
+                           move[1] + 1 != move[0] and move[1] - 6 != move[0]): #check up
                             move_forms_mill.append(move)
-                        if (self.pieces[move - 1] == player and self.pieces[move - 2] == player): #check right
+                        if (self.pieces[move[1] - 1] == player and self.pieces[move[1] - 2] == player and
+                           move[1] - 1 != move[0] and move[1] - 2 != move[0]): #check right
                             move_forms_mill.append(move)
                     else: #move is in the bottom or top right corner of a ring
-                        if (self.pieces[move + 1] == player and self.pieces[move + 2] == player): #check down/ left
+                        if (self.pieces[move[1] + 1] == player and self.pieces[move[1] + 2] == player and
+                           move[1] + 1 != move[0] and move[1] + 2 != move[0]): #check down/ left
                             move_forms_mill.append(move)
-                        if (self.pieces[move - 1] == player and self.pieces[move - 2] == player): #check left/ up
+                        if (self.pieces[move[1] - 1] == player and self.pieces[move[1] - 2] == player and
+                           move[1] - 1 != move[0] and move[1] - 2 != move[0]): #check left/ up
                             move_forms_mill.append(move)
                 
                 else: #move is in the middle of a row
-                    if move in [1,3,5,7]: #outer ring
-                        if move == 7:
-                            if (self.pieces[move - 7] == player and self.pieces[move - 1] == player): #check ring
+                    if move[1] in [1,3,5,7]: #outer ring
+                        if move[1] == 7:
+                            if (self.pieces[move[1] - 7] == player and self.pieces[move[1] - 1] == player and
+                               move[1] - 7 != move[0] and move[1] - 1 != move[0]): #check ring
                                 move_forms_mill.append(move)
                         else:
-                            if (self.pieces[move - 1] == player and self.pieces[move + 1] == player): #check ring
+                            if (self.pieces[move[1] - 1] == player and self.pieces[move[1] + 1] == player and
+                               move[1] - 1 != move[0] and move[1] + 1 != move[0]): #check ring
                                 move_forms_mill.append(move)
-                        if (self.pieces[move + 8] == player and self.pieces[move + 16] == player): #check intersections
+                        if (self.pieces[move[1] + 8] == player and self.pieces[move[1] + 16] == player and
+                           move[1] + 8 != move[0] and move[1] + 16 != move[0]): #check intersections
                                 move_forms_mill.append(move)
                                 
-                    elif move in [9,11,13,15]: #middle ring
-                        if move == 15:
-                            if (self.pieces[move - 7] == player and self.pieces[move - 1] == player): #check ring
+                    elif move[1] in [9,11,13,15]: #middle ring
+                        if move[1] == 15:
+                            if (self.pieces[move[1] - 7] == player and self.pieces[move[1] - 1] == player and
+                               and move[1] - 7 != move[0] and move[1] - 1 != move[0]): #check ring
                                 move_forms_mill.append(move)
                         else: 
-                            if (self.pieces[move - 1] == player and self.pieces[move + 1] == player): #check ring
+                            if (self.pieces[move[1] - 1] == player and self.pieces[move[1] + 1] == player and
+                               move[1] - 1 != move[0] and move[1] + 1 != move[0]): #check ring
                                 move_forms_mill.append(move)
-                        if (self.pieces[move + 8] == player and self.pieces[move - 8] == player): #check intersections
+                        if (self.pieces[move[1] + 8] == player and self.pieces[move[1] - 8] == player and
+                           move[1] + 8 != move[0] and move[1] - 8 != move[0]): #check intersections
                                 move_forms_mill.append(move)
                        
-                    elif move in [17,19,21,23]: #inner ring
-                        if move == 23:
-                            if (self.pieces[move - 7] == player and self.pieces[move - 1] == player): #check ring
+                    elif move[1] in [17,19,21,23]: #inner ring
+                        if move[1] == 23:
+                            if (self.pieces[move[1] - 7] == player and self.pieces[move[1] - 1] == player and
+                               move[1] - 7 != move[0] and move[1] - 1 != move[0]): #check ring
                                 move_forms_mill.append(move)
                         else: 
-                            if (self.pieces[move - 1] == player and self.pieces[move + 1] == player): #check ring
+                            if (self.pieces[move[1] - 1] == player and self.pieces[move[1] + 1] == player and
+                               move[1] - 1 != move[0] and move[1] + 1 != move[0]): #check ring
                                 move_forms_mill.append(move)
-                        if (self.pieces[move - 8] == player and self.pieces[move - 16] == player): #check intersections
+                        if (self.pieces[move[1] - 8] == player and self.pieces[move[1] - 16] == player and
+                           move[1] - 8 != move[0] and move[1] - 16 != move[0]): #check intersections
                                 move_forms_mill.append(move)
                 
         return list(move_forms_mill)
     
-    
+    """
+    Looks at the board and returns all current mills for a given player, in tuples of their coordinates
+    IDEA: maybe not in tuples, but in a set of coordinates
+    """
     def check_for_mills(self, player):
         
         current_mills = []
@@ -302,6 +327,43 @@ s
             index += 2
             
         return list(current_mills)
+    
+    """
+    given a position, this method returns a tuple with all the neighboring positions
+    """
+    def get_neighbours(self, position):
+        
+        if position != None:
+                if position % 2 == 0: #position is in a corner
+                    
+                    if position % 8 == 0: # position is in the top left corner of a ring
+                        return (positon + 1, position + 7)
+                    
+                    else: #position is in top right, or bottom corners
+                        return (position - 1, position + 1)
+                    
+                else: #position is in a intersection
+                    if position in [1,3,5,7]: #outer ring
+                        if position == 7:
+                            return (0, 6, 15)
+                        else:
+                            return (position + 1, position - 1, position + 8)
+                            
+                        
+                    elif position in [9,11,13,15]: #middle ring
+                        if position == 15:
+                            return (7, 8, 14, 23)
+                        else:
+                            return (position + 1, position - 1, position + 8, position - 8)
+                        
+                    elif position in [17,19,21,23]: #outer ring
+                        if position == 23:
+                            return (15, 16, 22)
+                        else:
+                            return (position + 1, position - 1, position - 8) 
+                    
+        
+        return
     
     """
     Looks at the board, given the current player and returns a list
@@ -332,7 +394,7 @@ s
     """
     def get_legal_moves_0(self, player):
         
-        #get player pieces TODO, CHECK FOR MILLS, IF ONLY MILLS, RETURN ALL
+        #get enemy pieces that can be taken if a mill is formed
         enemies_outside_mills = get_pieces_outside_mills(self, -player)
         if len(enemies_outside_mills) > 0:
             enemies_to_take = enemies_outside_mills
@@ -340,8 +402,10 @@ s
             enemies_to_take = get_player_pieces(self, -player)
         
         
-        #get empty positions
-        empty_locations = get_empty_positions(self)
+        #get empty positions, they represent all possible move locations for phase zero
+        empty_locations []
+        for position in get_empty_positions(self):
+            empty_locations.append(('none',position))
         
         #get moves -> for each move_location, check if a mill is formed (check row(s))
         mill_moves = get_possible_mills(self, empty_locations, player)
@@ -366,7 +430,30 @@ s
     legal moves for the current gamestate, given that the player is
     in Phase 1
     """
-    def get_legal_moves_1(self, color):
+    def get_legal_moves_1(self, player):
+        
+        #get enemy pieces that can be taken if a mill is formed
+        enemies_outside_mills = get_pieces_outside_mills(self, -player)
+        if len(enemies_outside_mills) > 0:
+            enemies_to_take = enemies_outside_mills
+        else:
+            enemies_to_take = get_player_pieces(self, -player)
+            
+            
+        current_positions = get_player_pieces(self, player)
+        
+        part_moves = []
+        
+        for position in current_positions:
+            neighbours = get_neighbours(self, position)
+            index = 0
+            for index < len(neighbours):
+                if self.pieces[neighbours[index]] == 0:
+                    part_moves.append(position, neighbours[index])
+                index += 1
+            
+        
+        
         
         return list(moves)
     
